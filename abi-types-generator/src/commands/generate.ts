6142c850d226c7fbe51e47f4f91f139086ba5610
Josh Stevens <joshstevens19@hotmail.co.uk>
2020-04-27 16:44:56 +0100

    handle typescript converter type

--- abi-types-generator/src/commands/generate.ts
+++ abi-types-generator/src/commands/generate.ts
@@ -2,6 +2,8 @@ import Helpers from '../common/helpers';
 import Logger from '../common/logger';
 import { IProgramOptions } from '../common/models/iprogram-options';
 import { ConverterType } from '../converters/enums/converter-type';
+import AbiGenerator from '../converters/typescript/abi-generator';
+import { Provider } from '../converters/typescript/enums/provider';
 import { CommandTypes } from './enums/command-types';
 
 export = {
@@ -10,6 +12,12 @@ export = {
     const abiPath = cmd.subcommands[0];
     const ouputPath = cmd.options.ouputPath || abiPath;
     const language = cmd.options.lang || ConverterType.ts;
+    const provider = (cmd.options.provider as Provider) || Provider.web3;
+
+    switch (language) {
+      case ConverterType.ts:
+        new AbiGenerator(provider);
+    }
 
     Logger.log(
       `successfully created typings for abi file ${abiPath} saved ${ouputPath}`
