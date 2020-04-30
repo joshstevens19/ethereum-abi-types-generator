aa53aac86f7dcaa29713578227f3a2afb6e18cca
Josh Stevens <joshstevens19@hotmail.co.uk>
2020-04-30 09:15:38 +0100

    fix unit tests

--- abi-types-generator/src/converters/typescript/ethers-factory.spec.ts
+++ abi-types-generator/src/converters/typescript/ethers-factory.spec.ts
@@ -18,6 +18,7 @@ describe('EthersFactory', () => {
       ).toEqual(
         Helpers.removeAllWhiteSpace(`
           import { ContractTransaction } from "ethers";
+          import { Arrayish, BigNumber, BigNumberish, Interface } from "ethers/utils";
           import { EthersContractContext } from "ethereum-abi-types-generator";
           export type ContractContext = EthersContractContext<
           TestAbi,
