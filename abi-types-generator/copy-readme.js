b12a40ebe558ffd53112284b166a4c8038cad904
Josh Stevens <joshstevens19@hotmail.co.uk>
2020-05-01 16:08:40 +0100

    remove images from package and edit the script to pull the readme across

--- abi-types-generator/copy-readme.js
+++ abi-types-generator/copy-readme.js
@@ -1,3 +1,16 @@
 const fs = require('fs');
 
-fs.createReadStream('../README.md').pipe(fs.createWriteStream('./README.md'));
+fs.readFile('../README.md', function (err, buf) {
+  if (err) console.log(err);
+  let data = buf.toString();
+  data = data.replace(
+    '<img src="./images/gif-demo.gif" />',
+    'Demo gif can be seen on the github readme'
+  );
+  console.log(data);
+
+  fs.writeFile('./README.md', data, (err) => {
+    if (err) console.log(err);
+    console.log('Successfully Updated the readme.');
+  });
+});
